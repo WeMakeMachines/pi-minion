e9eb7cd907b19b74b15291304cad783a0c6b17b2
Franco Speziali <franco@wemakemachines.com>
2021-07-05 21:49:45 +0200

    Added flask-cors

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from services import Caching
 from weather import weather
+from flask_cors import CORS
 
 from flask import Flask
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(weather)
