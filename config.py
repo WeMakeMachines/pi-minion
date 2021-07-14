5af5ae9bcc94ee9999836a1e2e181ca5154dbb29
Franco Speziali <franco@wemakemachines.com>
2021-07-14 14:48:49 +0200

    Formatting

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 
+
 class BaseConfig():
     OPEN_WEATHER_MAP_API_KEY = config["OPEN_WEATHER_MAP_API_KEY"]
     LATITUDE = config["LATITUDE"]
