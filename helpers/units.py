7e4403a3996ca9d280d0a769ce4d8526bc8c326b
Franco Speziali <franco@wemakemachines.com>
2021-07-16 18:42:33 +0200

    Added conversion formulae for units

--- helpers/units.py
+++ helpers/units.py
@@ -23,15 +23,31 @@ class Units(Enum):
 
 @unique
 class Speed(Enum):
-    KILOMETRES_PER_HOUR = "km/h"
     MILES_PER_HOUR = "miles/h"
+    KILOMETRES_PER_HOUR = "km/h"
 
     @staticmethod
-    def metres_per_second_to_km_per_hour(value):
+    def metres_per_second_to_km_per_hour(value: float):
         return round(value * 3.599712, 1)
+
+    @staticmethod
+    def as_imperial(value: float):
+        return round(value * 0.621477, 1)
+
+    @staticmethod
+    def as_metric(value: float):
+        return round(value * 1.609071, 1)
 
 
 @unique
 class Temperature(Enum):
-    CELSIUS = "celsius"
     FAHRENHEIT = "fahrenheit"
+    CELSIUS = "celsius"
+
+    @staticmethod
+    def as_imperial(value):
+        return round((value - 32) * 5 / 9, 1)
+
+    @staticmethod
+    def as_metric(value):
+        return round((value * 9 / 5) + 32, 1)
