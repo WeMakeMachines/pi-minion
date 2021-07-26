14c0b3daa02ea5044c6abcceec887457e4181a5d
Franco Speziali <franco@wemakemachines.com>
2021-07-26 23:32:07 +0200

    Fixed mapping error with Forecast

--- models/Weather/Forecast.py
+++ models/Weather/Forecast.py
@@ -2,4 +2,4 @@ class Forecast(dict):
     def __init__(self, main: str, description: str):
         self.main = main
         self.description = description
-        dict.__init__(self, sunrise=self.main, description=self.description)
+        dict.__init__(self, main=self.main, description=self.description)
