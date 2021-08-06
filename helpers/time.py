9931838348480f5d4da2d114a93976904adce66e
Franco Speziali <franco@wemakemachines.com>
2021-08-06 13:44:45 +0200

    Modified timedate comparison to use timedelta

--- helpers/time.py
+++ helpers/time.py
@@ -6,16 +6,25 @@ class DateTimeComparison:
         self.datetime = datetime.fromtimestamp(timestamp)
         self.now = datetime.now()
 
-    def has_hour_from_timestamp_passed(self):
+    def get_diff_in_minutes(self):
+        diff = (self.now - self.datetime)
+        diff_in_minutes = diff.total_seconds() / 60
 
-        if self.now.hour > self.datetime.hour:
+        return diff_in_minutes
+
+    def has_hour_from_timestamp_passed(self):
+        diff_in_minutes = self.get_diff_in_minutes()
+
+        if diff_in_minutes > 60:
             return True
 
         return False
 
     def has_day_from_timestamp_passed(self):
+        diff_in_minutes = self.get_diff_in_minutes()
+        diff_in_hours = diff_in_minutes / 60
 
-        if self.now.day > self.datetime.day:
+        if diff_in_hours > 24:
             return True
 
         return False
