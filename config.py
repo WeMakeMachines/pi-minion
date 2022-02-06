176347ceceb858e503aaeec9021977f6fcfc1521
Franco Speziali <franco@wemakemachines.com>
2022-02-06 19:26:25 +0100

    Refactored config to use new caching options

--- config.py
+++ config.py
@@ -1,32 +1,35 @@
-from app.helpers.cache import CacheExpiresAfter
-from app.helpers.units import Units
+from app.utils.CacheRequest import CacheBehaviour
+from app.utils.units import Units
 from configparser import ConfigParser
 
 config = ConfigParser()
 config.read('config.ini')
 
 
-def handle_cache_expires_after():
-    try:
-        if CacheExpiresAfter(config["cache"]["cache_expires_after"]) is CacheExpiresAfter.DISABLE:
-            return CacheExpiresAfter.DISABLE
-
-        if CacheExpiresAfter(config["cache"]["cache_expires_after"]) is CacheExpiresAfter.TODAY:
-            return CacheExpiresAfter.TODAY
-
-    except:
-        return int(config["cache"]["cache_expires_after"])
-
-
 class BaseConfigError(Exception):
     pass
 
 
+def handle_cache_behaviour():
+    try:
+        if CacheBehaviour(config["cache"]["cache_behaviour"]) is CacheBehaviour.DISABLE:
+            print("Caching is disabled")
+            return CacheBehaviour.DISABLE
+
+        if CacheBehaviour(config["cache"]["cache_behaviour"]) is CacheBehaviour.RENEW_DAILY:
+            print("Caching is set to renew daily")
+            return CacheBehaviour.RENEW_DAILY
+
+    except:
+        return CacheBehaviour.USER_SPECIFIED
+
+
 class BaseConfig:
     OPEN_WEATHER_MAP_API_KEY = config["api"]["open_weather_map_key"]
-    LATITUDE = config["general"]["latitude"]
-    LONGITUDE = config["general"]["longitude"]
-    CACHE_EXPIRES_AFTER = handle_cache_expires_after()
+    LATITUDE = config.getfloat("general", "latitude")
+    LONGITUDE = config.getfloat("general", "longitude")
+    CACHE_BEHAVIOUR = handle_cache_behaviour()
+    CACHE_EXPIRES_AFTER = config.getint("cache", "cache_expires_after") or 0
     DEFAULT_BASE_UNITS = Units.METRIC
     BASE_UNITS = DEFAULT_BASE_UNITS if config["general"]["base_units"] is None else Units(
         config["general"]["base_units"])
